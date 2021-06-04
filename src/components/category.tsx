2194a1ccc0d8a49f37c5acf9f8db8dee590772e6
Tomas Jaros <xjaros2@fi.muni.cz>
2021-06-04 20:12:07 +0200

    feat: added category component

--- src/components/category.tsx
+++ src/components/category.tsx
@@ -5,14 +5,14 @@ type Data = { imageLink: String; personName: String }
 
 const Category = ({ data, categoryName }) => {
   return (
-    <div className="flex flex-col pb-16 items-center">
-      <h1 className="text-5xl pb-10 font-semibold">{categoryName}</h1>
-      <div className="flex flex-col md:flex-row pb-10">
+    <div className="flex flex-col items-center pb-16">
+      <h1 className="pb-10 text-5xl font-semibold">{categoryName}</h1>
+      <div className="flex flex-col pb-10 md:flex-row">
         {data.slice(0, 4).map((e: Data) => {
           return <Portrait imageLink={e.imageLink} personName={e.personName} />
         })}
       </div>
-      <div className="flex flex-col md:flex-row pb-10">
+      <div className="flex flex-col pb-10 md:flex-row">
         {data.slice(4, 8).map((e: Data) => {
           return <Portrait imageLink={e.imageLink} personName={e.personName} />
         })}
