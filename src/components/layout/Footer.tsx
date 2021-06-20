7da1373d9f7f7acc8f07efb29e1d8f7fd3140adf
michal <michal.barnisin@gmail.com>
2021-06-20 22:17:45 +0200

    [feat] Separate links from the code, use .map() to generate same links.

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,39 +1,54 @@
-import React, { useState } from "react"
-import { FaFacebook, FaInstagram, FaGlobe } from "react-icons/fa"
+import React from 'react';
+import { FaFacebook, FaInstagram, FaGlobe } from 'react-icons/fa';
+import { IconType } from 'react-icons/lib';
 
-function Footer() {
+const footerLinks = [
+  { url: '', description: 'Contact Us' },
+  { url: '', description: 'Advertising' },
+  { url: '', description: 'Privacy Policy' },
+  { url: '', description: 'Terms Of Use' }
+];
+
+const socialIcons: Record<string, IconType> = {
+  facebook: FaFacebook,
+  instagram: FaInstagram,
+  website: FaGlobe
+};
+
+const socialMedia = [
+  { url: '', type: 'instagram' },
+  { url: '', type: 'facebook' },
+  { url: '', type: 'website' }
+];
+
+const Footer: React.FC = () => {
   return (
-    <div className="flex flex-col items-center text-white bg-black h-72">
-      <nav className="flex flex-row pt-12 justify-evenly">
-        <a href="" className="px-4 text-2xl text-white no-underline">
-          Contact Us
-        </a>
-        <a href="" className="px-4 text-2xl text-white no-underline">
-          Advertising
-        </a>
-        <a href="" className="px-4 text-2xl text-white no-underline">
-          Privacy Policy
-        </a>
-        <a href="" className="px-4 text-2xl text-white no-underline">
-          Terms Of Use
-        </a>
+    <footer className="flex flex-col items-center mt-auto text-white bg-black">
+      <nav className="flex flex-col items-center py-12 md:flex-row justify-evenly md:pb-0">
+        {footerLinks.map(({ url, description }) => (
+          <a
+            href={url}
+            className="py-2 text-2xl text-white no-underline md:py-0 md:px-4"
+            key={description}>
+            {description}
+          </a>
+        ))}
       </nav>
-      <nav className="flex flex-row pt-8 justify-evenly">
-        <a href="" className="px-4 text-2xl text-white no-underline">
-          <FaInstagram size="3rem" />
-        </a>
-        <a href="" className="px-4 text-2xl text-white no-underline">
-          <FaFacebook size="3rem" />
-        </a>
-        <a href="" className="px-4 text-2xl text-white no-underline">
-          <FaGlobe size="3rem"/>
-        </a>
+      <nav className="flex flex-row justify-center w-full bg-black md:pt-8">
+        {socialMedia.map(({ url, type }) => {
+          const Icon = socialIcons[type] || FaGlobe;
+          return (
+            <a key={type} href={url} className="px-4 text-2xl text-white no-underline">
+              <Icon size="3rem" />
+            </a>
+          );
+        })}
       </nav>
-      <span className="pt-14">
-      All content Copyright © 2000-2021 ??????.com, Inc.  made with big pepee by us.
-      </span>
-    </div>
-  )
-}
+      <p className="w-full px-4 py-12 text-center break-words bg-black md:pt-14">
+        All content Copyright © 2000-2021 ??????.com, Inc. made with big pepee by us.
+      </p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
