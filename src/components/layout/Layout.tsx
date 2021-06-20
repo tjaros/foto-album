307806177f776fe59e82a34ff62141e4e607188b
michal <michal.barnisin@gmail.com>
2021-06-20 21:35:53 +0200

    [feat] Simplify Layout.tsx.
    * remove unused graphQl,
    * simplify structure to header-main-footer,
    * change css style to tailwind css classes

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,44 +1,16 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import '/src/css/style.css';
 
-import Header from './Header'
-import '/src/css/style.css'
+const Layout: React.FC = ({ children }) => (
+  <>
+    <Header />
+    <main className="max-w-5xl px-4 m-auto lg:px-8">
+      {children}
+    </main>
+    <Footer />
+  </>
+);
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`
-        }}>
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
-
-export default Layout
+export default Layout;
