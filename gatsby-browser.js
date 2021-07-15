66b1e06667dedf8dc3307bf72a8be86c649fa027
Tomas Jaros <xjaros2@fi.muni.cz>
2021-07-15 17:18:54 +0200

    [feat] Wrap root with ApolloProvider && RecoilRoot

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,14 @@
  */
 
 // You can delete this file if you're not using it
+
+import React from 'react';
+import { RecoilRoot } from 'recoil';
+import { ApolloProvider } from '@apollo/client';
+import client from './src/apollo';
+
+export const wrapRootElement = ({ element }) => (
+  <ApolloProvider client={client}>
+    <RecoilRoot>{element}</RecoilRoot>
+  </ApolloProvider>
+);
