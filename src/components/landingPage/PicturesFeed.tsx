fad17024814997fe74e24e0921e6dd952d6315ca
Katarina Bulkova <kbulkova@redhat.com>
2021-07-16 23:22:03 +0200

    [add] landing page pictures feeding, panel for unregistered user

--- src/components/landingPage/PicturesFeed.tsx
+++ src/components/landingPage/PicturesFeed.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ColumnsLayout from "../ColumnsLayout";
 import { gql, useQuery } from '@apollo/client';
 import { FaTruckLoading } from "react-icons/fa";
-import client from '../../apollo';
 
 
 const Image = ({ url }: { url: string }) => (
@@ -13,11 +12,9 @@ const Image = ({ url }: { url: string }) => (
   
 
 interface modelAvatar {
-    model: {
         avatar: {
             url: string
         }
-    }
 }
 
 interface modelAvatarUrl {
@@ -28,7 +25,7 @@ const distinct = (avatar: modelAvatar[]) => {
 const output: modelAvatarUrl[] = [];
 avatar.forEach((item: modelAvatar) => {
     const obj = {
-    url: item.model.avatar.url
+    url: item.avatar.url
     };
     if (!output.some((x) => x.url === obj.url)) {
     output.push(obj);
@@ -40,15 +37,22 @@ return output;
   
 
 const PicturesFeed: React.FC = () => {
-    const gqlurls = gql`
+    const models_query = gql`
         query GET_ALL_AVATARS {
         models {
             avatar {
                 url
             }
+        },
+        photographers {
+            avatar {
+                url
+            }
         }
         }`;
-    const { loading, error, data} = useQuery(gqlurls);
+
+
+    const { loading, error, data} = useQuery(models_query);
 
     if (error) return <div>Failed to load avatars</div>
     if (loading) return <FaTruckLoading></FaTruckLoading>
@@ -62,22 +66,3 @@ const PicturesFeed: React.FC = () => {
 };
 
 export default PicturesFeed;
-  
-  
-  
-  // const urls = {JSON.stringify(gqlurls.data)}
-  
-    // console.log(urls);
-  
-  // const urls = [
-  //   'https://images.pexels.com/photos/1680342/pexels-photo-1680342.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
-  //   'https://images.pexels.com/photos/3435321/pexels-photo-3435321.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
-  //   'https://dummyimage.com/600x400/000/fff.png',
-  //   'https://dummyimage.com/800x1600/53c227/fff.png',
-  //   'https://dummyimage.com/600x400/000/c225c2.png',
-  //   'https://dummyimage.com/50x400/f2112b/fff.png',
-  //   'https://dummyimage.com/600x100/000/fff.png',
-  //   'https://dummyimage.com/800x400/53c227/fff.png',
-  //   'https://dummyimage.com/600x200/000/fff.png',
-  //   'https://dummyimage.com/600x400/000/c225c2.png'
-  // ];
