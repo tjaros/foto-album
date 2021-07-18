af22ba3e34e35de017b605d665c714015e1ca598
michal <michal.barnisin14@gmail.com>
2021-07-18 12:23:24 +0200

    [feat] Add modal to view images.

--- src/templates/Album.tsx
+++ src/templates/Album.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { graphql, Link, PageProps } from 'gatsby';
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css';
 import { ColumnsLayout, Layout, MetaData } from '../components';
 
 // Please note that you can use https://github.com/dotansimha/graphql-code-generator to generate all types from graphQL schema
@@ -80,23 +82,21 @@ const AlbumPageTemplate: React.FC<AlbumPageProps> = ({
     strapi: { album }
   }
 }) => {
-  console.log('Building', album);
+  const [photoIndex, setPhotoIndex] = useState(0);
+  const [isOverlayOpen, setOverlayOpen] = useState(false);
+  const nPhotos = album.photos.length;
+
+  console.log('rerender', photoIndex, isOverlayOpen);
+
   return (
     <Layout>
       <MetaData title={`Album ${album.name}`} />
       <p className="py-6 mx-6 text-6xl font-semibold">{album.name}</p>
       <ColumnsLayout nColumns={4}>
-        {album.photos.map(({ url }) => (
-          <img alt="" src={url} key={url} />
-        ))}
-        {album.photos.map(({ url }) => (
-          <img alt="" src={url} key={url} />
-        ))}
-        {album.photos.map(({ url }) => (
-          <img alt="" src={url} key={url} />
-        ))}
-        {album.photos.map(({ url }) => (
-          <img alt="" src={url} key={url} />
+        {album.photos.map(({ url }, index) => (
+          <button type="button" onClick={() => { setPhotoIndex(index); setOverlayOpen(true); }}>
+            <img alt="" src={url} key={url} />
+          </button>
         ))}
       </ColumnsLayout>
       <div className="flex flex-row justify-around py-12">
@@ -113,6 +113,16 @@ const AlbumPageTemplate: React.FC<AlbumPageProps> = ({
           roleAs="Model"
         />
       </div>
+      {nPhotos > 0 && isOverlayOpen && (
+        <Lightbox
+          mainSrc={album.photos[photoIndex].url}
+          nextSrc={album.photos[(photoIndex + 1) % nPhotos].url}
+          prevSrc={album.photos[(photoIndex + nPhotos - 1) % nPhotos].url}
+          onCloseRequest={() => setOverlayOpen(false)}
+          onMovePrevRequest={() => setPhotoIndex((photoIndex + nPhotos - 1) % nPhotos)}
+          onMoveNextRequest={() => setPhotoIndex((photoIndex + 1) % nPhotos)}
+        />
+      )}
     </Layout>
   );
 };
