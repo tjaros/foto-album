0eb52c645e0e3815fa0c6c907da8358a2bd830d1
michal <michal.barnisin14@gmail.com>
2021-07-20 00:30:38 +0200

    [feat] Create categories (static) page.

--- src/pages/categories.tsx
+++ src/pages/categories.tsx
@@ -1,19 +1,56 @@
+import { graphql, Link, PageProps } from 'gatsby';
 import React from 'react';
-import { Layout, MetaData, Category } from '../components';
+import { Layout, MetaData, Category, Portrait } from '../components';
 
-const categories = new Array(4).fill({
-  data: Array(8).fill({
-    imageLink: 'https://thispersondoesnotexist.com/image',
-    personName: 'Your Papa',
-  }),
-  categoryName: 'MEN',
-});
+export const query = graphql`
+  query GetModelsForCategories {
+    strapi {
+      categories {
+        name
+        models(limit: 8) {
+          name
+          slug
+          avatar {
+            url
+          }
+        }
+      }
+    }
+  }
+`;
 
-const Categories: React.FC = () => (
+interface CategoryPageProps extends PageProps {
+  data: {
+    strapi: {
+      categories: {
+        name: string;
+        models: {
+          name: string;
+          slug: string;
+          avatar: { url: string }
+        }[]
+      }[]
+    }
+  }
+}
+
+const Categories: React.FC<CategoryPageProps> = ({
+  data: {
+    strapi: { categories }
+  }
+}) => (
   <Layout>
     <MetaData title="Categories" />
     <div className="flex flex-col">
-      {categories.map((e) => (<Category data={e.data} categoryName={e.categoryName} />))}
+      {categories.map(({ name, models }) => (
+        <Category categoryName={name} key={name} className="pb-8 mt-8 md:pb-12 lg:pb-16">
+          {models.map((model) => (
+            <Link to={`/model/${model.slug}`}>
+              <Portrait imageLink={model.avatar.url} personName={model.name} imageClass="h-40 w-35 md:h-60 md:w-53 lg:h-80 lg:w-72" />
+            </Link>
+          ))}
+        </Category>
+      ))}
     </div>
   </Layout>
 );
