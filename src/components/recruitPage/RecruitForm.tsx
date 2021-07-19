179c04c5706ed2dd4f90fb16fd59bf4b3511450a
Katarina Bulkova <kbulkova@redhat.com>
2021-07-19 15:33:41 +0200

    [fix] validation in react hook form

--- src/components/recruitPage/RecruitForm.tsx
+++ src/components/recruitPage/RecruitForm.tsx
@@ -1,54 +1,53 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
-// const MAILGUN_API_KEY = '9530129bbd48e13f9f6d8a2f59957ba2-e31dc3cc-efe6709e';
+type UserSubmitForm = {
+  full_name: string;
+  age: number;
+  phone: number;
+  email: string;
+  self_description: string;
+};
 
-// const mailgun = require('mailgun-js')({
-//   apiKey: MAILGUN_API_KEY,
-//   domain: 'sandbox9291ca5b58a846e1afef446a86b32ad7.mailgun.org'
-// });
+const schema = yup.object().shape({
+  full_name: yup
+    .string()
+    .min(6, 'Fullname must be at least 6 characters')
+    .required('Fullname is required'),
+  age: yup.number().min(18, 'Min age is 18').max(80, 'Max age is 80').required('Age is required'),
+  phohe: yup.number().required('Phone is required'),
+  email: yup.string().required('Email is required').email('Email is invalid'),
+  self_description: yup
+    .string()
+    .min(3, 'Purpose must be at least 3 characters')
+    .required('Purpose is required')
+});
 
 const RecruitForm: React.FC = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset
+  } = useForm<UserSubmitForm>({
+    resolver: yupResolver(schema)
+  });
 
-  // eslint-disable-next-line no-console
-  const onSubmit = (data: unknown) => console.log({ data });
-  //   const data = {
-  //     from: 'Excited User <bulkova.katarina@gmail.com>',
-  //     to: 'bulkova.katarina@gmail.com',
-  //     subject: 'Hello',
-  //     text: 'Testing some Mailgun awesomeness!'
-  //   };
-
-  //   const sendData = () => {
-  //     mailgun.messages().send(data, ((error, body) => {
-  //       if (error) {
-  //         console.log(error);
-  //       }
-  //       console.log(body);
-  //     }));
-  //   };
-
-  //   const onSubmit = async (event: { preventDefault: () => void; }) => {
-  //     event.preventDefault(); // Prevent default submission
-  //     try {
-  //       await sendData();
-  //       alert('Your form was successfully submitted!');
-  //       setValues({
-  //         full_name: '', age: '', phone: '', email: '', self_description: ''
-  //       });
-  //     } catch (e) {
-  //       alert(`Registration failed! ${e.message}`);
-  //     }
-  //   };
+  const onSubmitHandler = (data: UserSubmitForm) => {
+    // eslint-disable-next-line no-console
+    console.log(data);
+    reset();
+  };
 
   return (
     <div className="flex items-center justify-center w-full border-black h-1/4 md:w-1/2 md:border-y-2">
       <div className="w-full p-8 bg-white shadow-lg">
         <h2 className="block w-full mb-6 text-center text-grey-darkest">Join us!</h2>
         <form
-          onSubmit={handleSubmit(onSubmit)}
+          onSubmit={handleSubmit(onSubmitHandler)}
           className="mb-4 space-y-4 text-xs md:flex md:flex-wrap md:justify-between">
           <div className="md:w-full center">
             <label className="block text-gray-600" htmlFor="full_name">
@@ -56,10 +55,9 @@ const RecruitForm: React.FC = () => {
             </label>
             <input
               className="w-full p-2 mt-1 bg-gray-200 border border-black"
-              type="text"
-              {...register('full_name', { required: true })}
-              id="full_name"
+              {...register('full_name')}
             />
+            <p>{errors.full_name?.message}</p>
           </div>
           <div className="md:w-full center">
             <label className="block text-gray-600" htmlFor="age">
@@ -67,10 +65,9 @@ const RecruitForm: React.FC = () => {
             </label>
             <input
               className="w-full p-2 mt-1 bg-gray-200 border border-black"
-              type="number"
-              {...register('age', { required: true, min: 18, max: 80 })}
-              id="age"
+              {...register('age')}
             />
+            <p>{errors.age?.message}</p>
           </div>
           <div className="md:w-full center">
             <label className="block text-gray-600" htmlFor="phone">
@@ -78,10 +75,9 @@ const RecruitForm: React.FC = () => {
             </label>
             <input
               className="w-full p-2 mt-1 bg-gray-200 border border-black"
-              type="text"
-              {...register('phone', { required: true })}
-              id="phone"
+              {...register('phone')}
             />
+            <p>{errors.phone?.message}</p>
           </div>
           <div className="md:w-full center">
             <label className="block text-gray-600" htmlFor="email">
@@ -89,9 +85,11 @@ const RecruitForm: React.FC = () => {
             </label>
             <input
               className="w-full p-2 mt-1 bg-gray-200 border border-black"
-              {...register('email', { required: true, pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/ })}
-              id="email"
+              {...register('email', {
+                pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/
+              })}
             />
+            <p>{errors.email?.message}</p>
           </div>
           <div className="md:w-full center">
             <label className="block text-gray-600" htmlFor="self_description">
@@ -100,9 +98,9 @@ const RecruitForm: React.FC = () => {
             <textarea
               className="w-full px-3 py-2 border border-black focus:outline-none bg-gray-200"
               rows={4}
-              {...register('self_description', { required: true })}
-              id="self_description"
+              {...register('self_description')}
             />
+            <p>{errors.self_description?.message}</p>
           </div>
           <button
             className="block p-4 mx-auto text-lg text-white uppercase bg-black rounded hover:bg-teal-dark"
--- src/components/registerPage/RegisterForm.tsx
+++ src/components/registerPage/RegisterForm.tsx
@@ -1,18 +1,42 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
-interface IFormInput {
+type UserSubmitForm = {
   full_name: string;
   email: string;
   self_description: string;
-}
+};
+
+const schema = yup.object().shape({
+  full_name: yup
+    .string()
+    .min(6, 'Fullname must be at least 6 characters')
+    .required('Fullname is required'),
+  email: yup.string().required('Email is required').email('Email is invalid'),
+  self_description: yup
+    .string()
+    .min(3, 'Purpose must be at least 3 characters')
+    .required('Purpose is required')
+});
 
 const RecruitForm: React.FC = () => {
-  const { register, handleSubmit } = useForm<IFormInput>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset
+  } = useForm<UserSubmitForm>({
+    resolver: yupResolver(schema)
+  });
 
-  // eslint-disable-next-line no-console
-  const onSubmit = (data: unknown) => console.log({ data });
+  const onSubmitHandler = (data: UserSubmitForm) => {
+    // eslint-disable-next-line no-console
+    console.log(data);
+    reset();
+  };
 
   return (
     <div className="flex items-center justify-center w-full border-black h-1/4 md:w-1/2 md:border-y-2">
@@ -24,18 +48,17 @@ const RecruitForm: React.FC = () => {
           Fill in the form and wait for confirmation email with your credentials
         </span>
         <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="mb-4 space-y-4 text-xs md:flex md:flex-wrap md:justify-between">
+          onSubmit={handleSubmit(onSubmitHandler)}
+          className="form-group mb-4 space-y-4 text-xs md:flex md:flex-wrap md:justify-between">
           <div className="md:w-full center">
             <label className="block text-gray-600" htmlFor="full_name">
               Full Name
             </label>
             <input
+              {...register('full_name')}
               className="w-full p-2 mt-1 bg-gray-200 border border-black"
-              type="text"
-              {...register('full_name', { required: true })}
-              id="full_name"
             />
+            <p>{errors.full_name?.message}</p>
           </div>
           <div className="md:w-full center">
             <label className="block text-gray-600" htmlFor="email">
@@ -43,9 +66,11 @@ const RecruitForm: React.FC = () => {
             </label>
             <input
               className="w-full p-2 mt-1 bg-gray-200 border border-black"
-              {...register('email', { required: true, pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/ })}
-              id="email"
+              {...register('email', {
+                pattern: /^[a-z0-9._%+-]=@[a-z0-9.-]+\.[a-z]{2,4}$/
+              })}
             />
+            <p>{errors.email?.message}</p>
           </div>
           <div className="md:w-full center">
             <label className="block text-gray-600" htmlFor="self_description">
@@ -54,9 +79,9 @@ const RecruitForm: React.FC = () => {
             <textarea
               className="w-full px-3 py-2 border border-black focus:outline-none bg-gray-200"
               rows={4}
-              {...register('self_description', { required: true })}
-              id="self_description"
+              {...register('self_description')}
             />
+            <p>{errors.self_description?.message}</p>
           </div>
           <button
             className="block p-4 mx-auto text-lg text-white uppercase bg-black rounded hover:bg-teal-dark"
